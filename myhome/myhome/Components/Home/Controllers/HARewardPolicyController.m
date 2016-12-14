722d9c505b8b77c1ac6968f4817885f40b14cd5c
fudon <fuart@qq.com>
2016-12-14 22:56:32 +0800

    可以借鉴但不能模仿

--- myhome/myhome/Components/Home/Controllers/HARewardPolicyController.m
+++ myhome/myhome/Components/Home/Controllers/HARewardPolicyController.m
@@ -17,8 +17,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.title = @"奖励政策";
-    // Do any additional setup after loading the view.
+    [self policyDesignViews];
 }
+
+- (void)policyDesignViews
+{
+    UILabel *timeLabel = [FSViewManager labelWithFrame:CGRectMake(WIDTHFC / 4, 10, WIDTHFC / 2, 30) text:@"2016年12月14日 22:25" textColor:[UIColor whiteColor] backColor:RGBCOLOR(220, 220, 220, 1) font:FONTFC(14) textAlignment:NSTextAlignmentCenter];
+    [self.scrollView addSubview:timeLabel];
+    self.scrollView.showsVerticalScrollIndicator = NO;
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(15, timeLabel.bottom + 30, 45, 45)];
+    imageView.image = IMAGENAMED(@"deviceInfo");
+    [self.scrollView addSubview:imageView];
+    
+    UILabel *label = [FSViewManager labelWithFrame:CGRectMake(imageView.right + 10, imageView.top, WIDTHFC - 100, 200) text:@"你知道吗？过去几个月的测试阶段，我已经和起晚个人类通过电话了，科幻电影吗？不，这已成现实，对我说【我要排队】" textColor:[UIColor blackColor] backColor:[UIColor whiteColor] font:FONTFC(14) textAlignment:NSTextAlignmentLeft];
+    label.numberOfLines = 0;
+    [label sizeToFit];
+    label.frame = CGRectMake(imageView.right + 10, imageView.top, WIDTHFC - 100, label.height + 20);
+    [self.scrollView addSubview:label];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
