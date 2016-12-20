b84ce384c23bf41a185af6a1c355c10eb696cf8e
fudongdong <fudongdong@guazi.com>
2016-12-20 10:50:29 +0800

    订单系统

---
+++ myhome/myhome/Components/OrderSystem/FSOrderSystemController.h
@@ -0,0 +1,20 @@
+//
+//  FSOrderSystemController.h
+//  myhome
+//
+//  Created by fudon on 2016/12/20.
+//  Copyright © 2016年 fuhope. All rights reserved.
+//
+
+/*
+    1.短信文字下单，【不要编辑分隔符】
+    2.邮件图片下单
+    3.电话下单，直接拨打电话
+    4.
+ */
+
+#import "FSBaseController.h"
+
+@interface FSOrderSystemController : FSBaseController
+
+@end
---
+++ myhome/myhome/Components/OrderSystem/FSOrderSystemController.m
@@ -0,0 +1,37 @@
+//
+//  FSOrderSystemController.m
+//  myhome
+//
+//  Created by fudon on 2016/12/20.
+//  Copyright © 2016年 fuhope. All rights reserved.
+//
+
+#import "FSOrderSystemController.h"
+
+@interface FSOrderSystemController ()
+
+@end
+
+@implementation FSOrderSystemController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
