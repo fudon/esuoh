4283932132cfb81ac7929787684651025e126259
fudon <fuart@qq.com>
2017-01-08 10:59:30 +0800

    实现屏幕截图功能

--- myhome/myhome/AppDelegate+Handler.m
+++ myhome/myhome/AppDelegate+Handler.m
@@ -10,6 +10,7 @@
 #import "FSMacro.h"
 #import "FSShareManager.h"
 #import "ARTabBarController.h"
+#import "FSImagePicker.h"
 
 @implementation AppDelegate (Handler)
 
@@ -20,7 +21,8 @@
                                                        queue:[NSOperationQueue mainQueue]
                                                   usingBlock:^(NSNotification *note) {
                                                       [FuData alertViewWithTitle:@"截屏通知" message:@"可以把图片发送到微信" btnTitle:@"发送" handler:^(UIAlertAction *action) {
-                                                          UIImage *image = note.object;
+                                                          UIImage *image = [FSImagePicker theNewestImageFromAlbum];
+//                                                          UIImage *image = [self imageWithScreenshot];
                                                           if ([image isKindOfClass:[UIImage class]]) {
                                                               [FSShareManager wxImageShareActionWithImage:image controller:nil result:^(NSString *bResult) {
                                                                   [FuData showMessage:bResult];
@@ -36,5 +38,64 @@
     ARTabBarController *tbController = [[ARTabBarController alloc] initWithClasses:array titles:titles types:types selectedColor:APPCOLOR];
     self.window.rootViewController = tbController;
 }
+
+- (NSData *)dataWithScreenshotInPNGFormat
+{
+    CGSize imageSize = CGSizeZero;
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    if (UIInterfaceOrientationIsPortrait(orientation))
+        imageSize = [UIScreen mainScreen].bounds.size;
+    else
+        imageSize = CGSizeMake([UIScreen mainScreen].bounds.size.height, [UIScreen mainScreen].bounds.size.width);
+    
+    UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    for (UIWindow *window in [[UIApplication sharedApplication] windows])
+    {
+        CGContextSaveGState(context);
+        CGContextTranslateCTM(context, window.center.x, window.center.y);
+        CGContextConcatCTM(context, window.transform);
+        CGContextTranslateCTM(context, -window.bounds.size.width * window.layer.anchorPoint.x, -window.bounds.size.height * window.layer.anchorPoint.y);
+        if (orientation == UIInterfaceOrientationLandscapeLeft)
+        {
+            CGContextRotateCTM(context, M_PI_2);
+            CGContextTranslateCTM(context, 0, -imageSize.width);
+        }
+        else if (orientation == UIInterfaceOrientationLandscapeRight)
+        {
+            CGContextRotateCTM(context, -M_PI_2);
+            CGContextTranslateCTM(context, -imageSize.height, 0);
+        } else if (orientation == UIInterfaceOrientationPortraitUpsideDown) {
+            CGContextRotateCTM(context, M_PI);
+            CGContextTranslateCTM(context, -imageSize.width, -imageSize.height);
+        }
+        if ([window respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)])
+        {
+            [window drawViewHierarchyInRect:window.bounds afterScreenUpdates:YES];
+        }
+        else
+        {
+            [window.layer renderInContext:context];
+        }
+        CGContextRestoreGState(context);
+    }
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return UIImagePNGRepresentation(image);
+}
+
+/**
+ *  返回截取到的图片
+ *
+ *  @return UIImage *
+ */
+- (UIImage *)imageWithScreenshot
+{
+    NSData *imageData = [self dataWithScreenshotInPNGFormat];
+    return [UIImage imageWithData:imageData];
+}
+
 
 @end
