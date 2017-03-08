e6698fe26844a9d2867d2fc43bfc6e4cf3be96c2
fudongdong <fudongdong@guazi.com>
2017-03-08 12:18:26 +0800

    new

---
+++ myhome/myhome/H5/FSSafeData.h
@@ -0,0 +1,19 @@
+//
+//  FSSafeData.h
+//  myhome
+//
+//  Created by fudon on 2017/3/8.
+//  Copyright © 2017年 fuhope. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FSSafeData : NSObject
+
++ (NSArray *)allLocations;
+
++ (NSArray *)birthdayArray;
+
++ (NSArray *)bankNumbers;
+
+@end
---
+++ myhome/myhome/H5/FSSafeData.m
@@ -0,0 +1,28 @@
+//
+//  FSSafeData.m
+//  myhome
+//
+//  Created by fudon on 2017/3/8.
+//  Copyright © 2017年 fuhope. All rights reserved.
+//
+
+#import "FSSafeData.h"
+
+@implementation FSSafeData
+
++ (NSArray *)allLocations
+{
+    return nil;
+}
+
++ (NSArray *)birthdayArray
+{
+    return nil;
+}
+
++ (NSArray *)bankNumbers
+{
+    return nil;
+}
+
+@end
